2cbc6416f3d770963ab503eec0b021b47ab08617
Süheyl Akagündüz <suheylakagunduzz@gmail.com>
2024-03-08 05:08:20 +0300

    first

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    // type inference nedir? (tip çıkarımı) bir değişken tipini ide tarafından bilinmesi durumudur.
+    val name: String = "Süheyl"
+    val surName: String
+    var age: Int
+    // val ile var farkı immutable and mutable, değiştirilemez ve değiştirilebilir değişken.
+    // aralarindaki performans farki yok denecek kadar azdir. Ancak val daha maliyetlidir(matematiksel olarak)
+
+}
